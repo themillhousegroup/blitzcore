a2b19eece2250f9df2dfce546f1436dc8e221d7a
themillhousegroup <john.marshall.au@gmail.com>
2025-02-14 12:35:19 +1100

    Update Counter.svelte

--- src/routes/Counter.svelte
+++ src/routes/Counter.svelte
@@ -11,6 +11,10 @@
 </script>
 
 <div class="counter">
+	<button onclick={() => {}} aria-label="Start a new game">
+		New Game
+	</button>
+
 	<button onclick={() => (count.target -= 1)} aria-label="Decrease the counter by one">
 		<svg aria-hidden="true" viewBox="0 0 1 1">
 			<path d="M0,0.5 L1,0.5" />
