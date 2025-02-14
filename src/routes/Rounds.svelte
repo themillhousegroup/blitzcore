3930747df9185661bb889632f0d4d8237f72ca05
themillhousegroup <john.marshall.au@gmail.com>
2025-02-14 14:50:03 +1100

    Create Rounds.svelte

---
+++ src/routes/Rounds.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import { Spring } from 'svelte/motion';
+
+  type Props = {
+    onNewGame: () => void;
+  }
+  const { onNewGame }: Props = $props();
+
+	const count = new Spring(0);
+	const offset = $derived(modulo(count.current, 1));
+
+	function modulo(n: number, m: number) {
+		// handle negative numbers
+		return ((n % m) + m) % m;
+	}
+</script>
+
+<div class="rounds">
+	
+</div>
+
+<style>
+	.rounds {
+		display: flex;
+    flex-direction: column;
+		border-top: 1px solid rgba(0, 0, 0, 0.1);
+		border-bottom: 1px solid rgba(0, 0, 0, 0.1);
+		margin: 1rem 0;
+    background-color: green;
+	}
+</style>
