62ee5ccf0e4e687e31c6ed5406792cc69c3d4b36
themillhousegroup <john.marshall.au@gmail.com>
2025-02-14 14:01:30 +1100

    Create Blitzcore.svelte

---
+++ src/routes/Blitzcore.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { Spring } from 'svelte/motion';
+  import Counter from './Counter.svelte';
+
+	function onNewGame() {
+    console.log(`NewGame`);
+	}
+</script>
+
+<div class="blitzcore">
+  <Counter onNewGame />
+</div>
+
+<style>
+  .blitzcore {
+  }
+</style>
